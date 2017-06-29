c4c4cbd4b6a95fd5f84a044886b77c5111f033e6
dcsprog <dcsprog@10.0.2.15>
2017-06-29 14:12:51 +0100

    user

--- application/model/User.php
+++ application/model/User.php
@@ -6,7 +6,66 @@ class User
     private $user_id;
     private $user_name;
     private $user_email;
+    
     public function __construct()
-    {}
+    {
+        
+    }
+    
+    public function __construct($user_id, $user_name,$user_email){
+        $this->$user_id    = $user_id;
+        $this->$user_name  = $user_name;
+        $this->$user_email = $user_email;
+    }
+    
+    /**
+     * @return the $user_id
+     */
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return the $user_name
+     */
+    public function getUser_name()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * @return the $user_email
+     */
+    public function getUser_email()
+    {
+        return $this->user_email;
+    }
+
+    /**
+     * @param field_type $user_id
+     */
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @param field_type $user_name
+     */
+    public function setUser_name($user_name)
+    {
+        $this->user_name = $user_name;
+    }
+
+    /**
+     * @param field_type $user_email
+     */
+    public function setUser_email($user_email)
+    {
+        $this->user_email = $user_email;
+    }
+
+    
 }
 
